83bc4935b77938977e5a78328a34e79f436e09f1
Moshiur Rahman <mrh6627@gmail.com>
2022-08-15 19:48:50 +0600

    Sleep night entity is created sucessfully

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/* sleep night Entity*/
 package com.example.android.trackmysleepquality.database
 
 import androidx.room.ColumnInfo
